8d4b02476323a97e1485719f90094971e0e6fcf4
goeun441 <120786130+Jang-Goeun@users.noreply.github.com>
2025-05-03 17:20:28 +0900

    refactor: ExpenseCalendar 파일 위치 변경

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import './App.css';
-import MainHeader from './components/header/MainHeader';
+import "./App.css";
+import MainHeader from "./components/header/MainHeader";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ChallengePage from "./pages/challenge/ChallengePage";
-import SignUpPageChallengePage from "./pages/challenge/SignUpChallengePage"
+import SignUpPageChallengePage from "./pages/challenge/SignUpChallengePage";
 import MeddlePage from "./pages/meddling/MeddlingPage";
-import ExpenseCalendar from './pages/challenge/ExpenseCalendar';
-import './App.css';
-import SignUp from './pages/login/SignUp';
-import Login from './pages/login/Login';
+import ExpenseCalendar from "./pages/expense/ExpenseCalendar";
+import "./App.css";
+import SignUp from "./pages/login/SignUp";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
     <Router>
       <MainHeader />
       <Routes>
-        <Route path='/meddling' element={<MeddlePage/>} />
+        <Route path="/meddling" element={<MeddlePage />} />
         <Route path="/challenges" element={<ChallengePage />} />
         <Route
           path="/challenges/signup-challenge"
