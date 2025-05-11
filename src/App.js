68d5a8aa6e8df4c69ced0129a8f7577df69e2331
goeun441 <120786130+Jang-Goeun@users.noreply.github.com>
2025-05-11 14:42:20 +0900

    feat: 네비바 소비일기 드롭다운 구현

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
-import './App.css';
-import MainHeader from './components/header/MainHeader';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ChallengePage from './pages/challenge/ChallengePage';
-import SignUpPageChallengePage from './pages/challenge/SignUpChallengePage';
-import ProductPage from './pages/product/ProductPage';
-import ProductDetailPage from './pages/product/ProductDetailPage';
-import MeddlePage from './pages/meddling/MeddlingPage';
-import ExpenseCalendar from './pages/expense/ExpenseCalendar';
-import './App.css';
-import SignUp from './pages/login/SignUp';
-import Login from './pages/login/Login';
-import Expense from './pages/expense/Expense';
-import ChallengeDetailPage from './pages/challenge/ChallengeDetailPage';
-import MyWishPage from './pages/wish/MyWishPage';
-import FriendWishPage from './pages/wish/FriendWishPage';
-import AddWishExistPage from './pages/wish/AddWishExistPage';
-import GlobalStyle from './styles/GlobalStyle';
-import Category from './pages/onboarding/Category';
-import Goal from './pages/onboarding/Goal';
-import MyProfile from './pages/myprofile/MyProfile';
-import EditProfile from './pages/myprofile/EditProfile';
-import SavedChallenge from './pages/myprofile/SavedChallenge';
-import SearchFreindPage from './pages/friend/SearchFreindPage';
-import FriendProfile from './pages/friend/FriendProfile';
-import FreindsPage from './pages/friend/FriendsPage';
-import FriendsPage from './pages/friend/FriendsPage';
+import "./App.css";
+import MainHeader from "./components/header/MainHeader";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ChallengePage from "./pages/challenge/ChallengePage";
+import SignUpPageChallengePage from "./pages/challenge/SignUpChallengePage";
+import ProductPage from "./pages/product/ProductPage";
+import ProductDetailPage from "./pages/product/ProductDetailPage";
+import MeddlePage from "./pages/meddling/MeddlingPage";
+import ExpenseCalendar from "./pages/expense/ExpenseCalendar";
+import "./App.css";
+import SignUp from "./pages/login/SignUp";
+import Login from "./pages/login/Login";
+import Expense from "./pages/expense/Expense";
+import ChallengeDetailPage from "./pages/challenge/ChallengeDetailPage";
+import MyWishPage from "./pages/wish/MyWishPage";
+import FriendWishPage from "./pages/wish/FriendWishPage";
+import AddWishExistPage from "./pages/wish/AddWishExistPage";
+import GlobalStyle from "./styles/GlobalStyle";
+import Category from "./pages/onboarding/Category";
+import Goal from "./pages/onboarding/Goal";
+import MyProfile from "./pages/myprofile/MyProfile";
+import EditProfile from "./pages/myprofile/EditProfile";
+import SavedChallenge from "./pages/myprofile/SavedChallenge";
+import SearchFreindPage from "./pages/friend/SearchFreindPage";
+import FriendProfile from "./pages/friend/FriendProfile";
+import FreindsPage from "./pages/friend/FriendsPage";
+import FriendsPage from "./pages/friend/FriendsPage";
+import ExpenseAnalysis from "./pages/expense/ExpenseAnalysis";
 
 function App() {
   return (
@@ -38,8 +39,9 @@ function App() {
           path="/challenges/signup-challenge"
           element={<SignUpPageChallengePage />}
         />
-        <Route path="/expenseCalendar" element={<ExpenseCalendar />} />
+        <Route path="/expense-calendar" element={<ExpenseCalendar />} />
         <Route path="/expense" element={<Expense />} />
+        <Route path="/expense-analysis" element={<ExpenseAnalysis />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/products" element={<ProductPage />} />
---
+++ src/pages/expense/ExpenseAnalysis.jsx
@@ -0,0 +1,5 @@
+const ExpenseAnalysis = () => {
+  return <></>;
+};
+
+export default ExpenseAnalysis;
